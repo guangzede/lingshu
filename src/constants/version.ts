f4ddc12f3f0cf080ec249620c246675ffd68b090
倪达烨 <530070136@qq.com>
2026-02-03 07:28:39 +0800

    chore(version): update version and build time to latest values

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,15 +1,15 @@
 /**
  * 自动生成的版本号文件
- * 生成时间: 2026-02-02T13:29:44.414Z
+ * 生成时间: 2026-02-02T22:49:15.168Z
  */
 
-export const APP_VERSION = '1.0.0-20260202_212944';
-export const BUILD_TIME = '20260202_212944';
+export const APP_VERSION = '1.0.0-20260203_064915';
+export const BUILD_TIME = '20260203_064915';
 export const BASE_VERSION = '1.0.0';
 
 export const getVersionInfo = () => ({
   version: APP_VERSION,
   buildTime: BUILD_TIME,
   baseVersion: BASE_VERSION,
-  timestamp: new Date('2026-02-02T13:29:44.414Z').getTime(),
+  timestamp: new Date('2026-02-02T22:49:15.169Z').getTime(),
 });
