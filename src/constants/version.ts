5f90fb659044906671e2c26b030f4079cca3d4f7
倪达烨 <530070136@qq.com>
2026-02-05 22:45:50 +0800

    feat: update index animation flow

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,15 +1,15 @@
 /**
  * 自动生成的版本号文件
- * 生成时间: 2026-02-05T11:02:48.618Z
+ * 生成时间: 2026-02-05T13:14:44.760Z
  */
 
-export const APP_VERSION = '1.0.0-20260205_190248';
-export const BUILD_TIME = '20260205_190248';
+export const APP_VERSION = '1.0.0-20260205_211444';
+export const BUILD_TIME = '20260205_211444';
 export const BASE_VERSION = '1.0.0';
 
 export const getVersionInfo = () => ({
   version: APP_VERSION,
   buildTime: BUILD_TIME,
   baseVersion: BASE_VERSION,
-  timestamp: new Date('2026-02-05T11:02:48.618Z').getTime(),
+  timestamp: new Date('2026-02-05T13:14:44.760Z').getTime(),
 });
